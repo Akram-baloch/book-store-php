b57b5528e49c3705c0e509b30fcf480a525bb973
Akram-baloch <akramb458@gmail.com>
2023-09-28 07:13:03 -0700

    I have added the footer

--- book-store/layouts/navbar.php
+++ book-store/layouts/navbar.php
@@ -1,39 +1,38 @@
 <nav class="navbar navbar-expand-lg shadow p-4 bg-body rounded me-5 ms-5 mt-3 navbar-light bg-light sticky-top">
-    <div class="container-fluid">
+  <div class="container-fluid">
 
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mx-auto w-100 align-items-center d-flex justify-content-around">
-          <li class="nav-item ">
-            <a class="nav-link fw-bold" aria-current="page" href="">HOME</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link fw-bold" aria-current="page" href="#">FEATURE</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link fw-bold" aria-current="page" href="">PAGES</a>
-          </li>
-          <img src="../assets/images/logo-dark.png" alt="">
-          <li class="nav-item">
-            <a class="nav-link fw-bold" aria-current="page" href="">ABOUT</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link fw-bold" aria-current="page" href="">CONTACT</a>
-          </li>
-          <div class="d-flex gap-2">
-            <a class="text-decoration-none" href="">
-              <span id="cartCountSpan" class="cart-count"></span>
-              <i class="bi-cart3 text-dark h5"></i>
-            </a>
-            <i class="bi bi-search mx-2 h5"></i>
-            <a href="../Assets/register.html">
-              <i class="bi-person text-dark h5"></i>
-            </a>
-          </div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mx-auto w-100 align-items-center d-flex justify-content-around">
+        <li class="nav-item ">
+          <a class="nav-link fw-bold" aria-current="page" href="#">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link fw-bold" aria-current="page" href="#">FEATURE</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link fw-bold" aria-current="page" href="#">PAGES</a>
+        </li>
+        <img src="../assets/images/logo-dark.png" alt="Image not found">
+        <li class="nav-item">
+          <a class="nav-link fw-bold" aria-current="page" href="#">ABOUT</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link fw-bold" aria-current="page" href="#">CONTACT</a>
+        </li>
+        <div class="d-flex gap-2">
+          <a class="text-decoration-none" href="">
+            <span id="cartCountSpan" class="cart-count"></span>
+            <i class="bi-cart3 text-dark h5"></i>
+          </a>
+          <i class="bi bi-search mx-2 h5"></i>
+          <a href="#">
+            <i class="bi-person text-dark h5"></i>
+          </a>
+        </div>
 
-      </div>
     </div>
-  </nav>
+  </div>
+</nav>
